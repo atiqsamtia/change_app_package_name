5ff72fc60ad171d3566e65ac7c1c95c3de0c89c6
Esmaeil Ahmadipour <software8686@gmail.com>
2022-06-09 14:29:19 +0430

    add more documents & remove deleteOldDirectories method (unused).

--- lib/change_app_package_name.dart
+++ lib/change_app_package_name.dart
@@ -2,7 +2,9 @@ library change_app_package_name;
 
 import './android_rename_steps.dart';
 
+/// [ChangeAppPackageName] this is main class of package and rename current package name.
 class ChangeAppPackageName {
+  /// [start] this method get new package name and start renaming package name steps.
   static void start(List<String> arguments) {
     if (arguments.isEmpty) {
       print('New package name is missing in paraments. please try again.');
